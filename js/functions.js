b40497138343127ee85bd880485acfb44ef8e2a5
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-02-19 18:51:35 +0300

    Написала часть дз

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,22 @@ function isPalindrome(string) {
 }
 
 isPalindrome('topot');
+
+
+//Функция, которая принимает три параметра исходную (строку, мин длину и строку с добавочными симв)
+var myPad = (source, count, addition) => {
+  if (source.length >= count) {
+    return source;
+  }
+
+  const preffixLength = count - source.length;
+	const sample = addition;
+
+  while (addition.length < preffixLength - sample.length) {
+    addition += addition;
+  }
+
+  return  sample.slice(0, preffixLength - addition.length ) + addition + source;
+};
+
+myPad('qwerty', 4, '0');

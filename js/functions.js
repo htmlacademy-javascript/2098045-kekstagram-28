65ff07bd6ee1ce7305bd9e305841095b9cb7e98b
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-02-20 13:12:31 +0300

    поудаляла пустые строки

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,10 @@
 // Длина строки
-
 let stringLenth = function (boolean, number) {
   if (boolean.length <= number) {
     return true;
   }
     return false;
 };
-
 stringLenth('проверяемая строка', 10);
 
 //Функция для проверки, является ли строка палиндромом
@@ -20,16 +18,11 @@ function isPalindrome(string) {
   const reversedString = newArray.join('');
   return string === reversedString;
 }
-
 isPalindrome('topot');
 
-
 //Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
-
 let extractNumber = function (string) {
-
   let newStr = string.replace(/\D/g,'');
-
   if (newStr > 0) {
   newStr = Number(newStr);
   console.log(newStr);
@@ -39,14 +32,11 @@ let extractNumber = function (string) {
   }
   extractNumber('1 кефир, 0.5 батона');
 
-
-
 //Функция, которая принимает три параметра исходную (строку, мин длину и строку с добавочными симв)
 var myPad = (source, count, addition) => {
   if (source.length >= count) {
     return source;
   }
-
   const preffixLength = count - source.length;
 	const sample = addition;
 

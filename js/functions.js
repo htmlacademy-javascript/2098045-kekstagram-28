26c9ee48830e3b50136dbc2d543f31f949167dab
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-02-25 19:14:35 +0300

    Поправила по линту)

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 // Длина строки
-let stringLenth = function (boolean, number) {
+const stringLenth = function (boolean, number) {
   return boolean.length <= number;
-}
+};
 stringLenth('проверяемая строка', 10);
 
 //Функция для проверки, является ли строка палиндромом
@@ -9,7 +9,7 @@ function isPalindrome(string) {
   string = string.toLowerCase();
   const symbols = [...string];
   const newSymbols = [];
-  symbols.forEach(element=> {
+  symbols.forEach((element)=> {
     newSymbols.unshift(element);
   });
   const reversedString = newSymbols.join('');
@@ -18,26 +18,24 @@ function isPalindrome(string) {
 isPalindrome('topot');
 
 //Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
-let extractNumber = function (string) {
+const extractNumber = function (string) {
   let newStr = string.replace(/\D/g,'');
   if (newStr > 0) {
-  newStr = Number(newStr);
-  return newStr;
+    newStr = Number(newStr);
+    return newStr;
   } else {
     return NaN;
-    };
   }
-  extractNumber('1 кефир, 0.5 батона');
+};
+extractNumber('1 кефир, 0.5 батона');
 
 //Функция, которая принимает три параметра исходную (строку, мин длину и строку с добавочными симв)
-const  formatString = (string, minLength, addition) => {
+const formatString = (string, minLength, addition) => {
   const actualPad = minLength - string.length;
 
   if (actualPad <= 0) {
     return string;
   }
-
-  return addition.slice(0,  formatString % addition.length) + addition.repeat( formatString / addition.length ) + string;
-}
-
-myPad('qwerty', 4, '0');
+  return addition.slice(0, formatString % addition.length) + addition.repeat(formatString / addition.length) + string;
+};
+formatString('qwerty', 4, '0');

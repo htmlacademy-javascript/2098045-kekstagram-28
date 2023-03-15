14a08e4ab169d7b01481ac3e19b56907bbc3e014
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-03-15 17:51:22 +0300

    Поправила комментарии

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -5,18 +5,18 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const pictureContainer = document.querySelector('.pictures');
 
 //кол-во фотографий
-const createThumbnails = createNewPhotos(25);
+const thumbnailElement = createNewPhotos(25);
 
 //создаю временный ящик
 const pictureContainerFragment = document.createDocumentFragment();
 
 //клонирую найденный шаблон и в нем создаю миниатюры
-createThumbnails.forEach (({url, description, comments, likes}) => {
+thumbnailElement.forEach (({url, description, comments, likes}) => {
   const thumbnail = pictureTemplate.cloneNode(true);
 
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
-  thumbnail.querySelector('.picture__comments').textContent = comments;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__likes').textContent = likes;
 
   pictureContainer.append(thumbnail);

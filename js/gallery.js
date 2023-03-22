f324f06b51da2fcd2c7e4070391cdcf4ed682b5c
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-03-22 16:15:46 +0300

    Поправила добавление класса modal-open

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,6 @@ import {showBigPicture, createComments} from './big-picture.js';
 const container = document.querySelector('.pictures');
 const commentCount = document.querySelector('.social__comment-count');
 const commentLoader = document.querySelector('.comments-loader');
-const pageBody = document.body;
 
 const renderGallery = (pictures) => {
   container.addEventListener('click', (evt) => {
@@ -14,7 +13,7 @@ const renderGallery = (pictures) => {
     const picture = pictures.find ((item) => item.id === Number(thumbnail.dataset.id));
     commentCount.classList.add('hidden'); //временно скрываю кол-во комментов
     commentLoader.classList.add('hidden'); //временно скрываю загрузу остальным комментов
-    pageBody.classList.add('modal-open');
+    document.body.classList.add('modal-open');
 
     showBigPicture(picture);
     createComments(picture.comments);

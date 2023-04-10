c929c3d0f69f5d7fc0dd72f3e2205e48d28c4a7b
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-04-10 13:06:57 +0300

    Добавила фото пользователя

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,26 +1,8 @@
 //нашла место для размещения фото и шаблон с содержимым
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-// const container = document.querySelector('.pictures');
 
 
 //клонирую найденный шаблон и в нем создаю миниатюры
-
-// createThumbnails = (picture) => {
-
-//   //создаю временный ящик
-//   picture.forEach (({url, description, comments, likes, id}) => {
-//     const thumbnail = pictureTemplate.cloneNode(true);
-
-//     thumbnail.querySelector('.picture__img').src = url;
-//     thumbnail.querySelector('.picture__img').alt = description;
-//     thumbnail.querySelector('.picture__comments').textContent = comments.length;
-//     thumbnail.querySelector('.picture__likes').textContent = likes;
-//     thumbnail.dataset.id = id;
-
-//     return (thumbnail);
-
-//   });
-// };
 
 const createThumbnails = ({comments, description, likes, url, id}) => {
   const thumbnail = pictureTemplate.cloneNode(true);

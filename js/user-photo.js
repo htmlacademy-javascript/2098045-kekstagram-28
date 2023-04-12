f09a34b83b4736278cca27b47ed79b069dd9c6d6
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-04-12 10:18:06 +0300

    навожу красоту

--- js/user-photo.js
+++ js/user-photo.js
@@ -7,7 +7,7 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  const matches = FILE_TYPES.some((element) => fileName.endsWith(element));
   if (matches) {
     preview.src = URL.createObjectURL(file);
     previewEffects.forEach ((miniPreview) => {
